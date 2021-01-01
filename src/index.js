ae65bc076565cb17cda05aa377ebe7c201e9e3cd
Wael wahbeh <waelio@users.noreply.github.com>
2021-01-01 20:07:50 +0200

    missing folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 function extendConf (conf) {
     conf.boot.push("~quasar-app-extension-waelio/src/boot/register.js");
     conf.build.transpileDependencies.push(/quasar-app-extension-waelio[\\/]src/);
-    
+
     conf.framework.config.dark = "auto";
     conf.framework.plugins.push("Notify", "Loading", "Dialog", "Meta", "Cookies", "LocalStorage", "SessionStorage");
     conf.build.vueRouterMode = "history";
@@ -23,6 +23,7 @@ function extendConf (conf) {
     conf.pages.push("~quasar-app-extension-waelio/src/pages/Terms.vue");
     conf.pages.push("~quasar-app-extension-waelio/src/pages/Settings.vue");
 }
+
 module.exports = function (api) {
   api.compatibleWith("quasar", "^1.1.1");
   api.compatibleWith("@quasar/app", "^1.1.0 || ^2.0.0");
