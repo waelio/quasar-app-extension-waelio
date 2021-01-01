6e250f6e6ed955d07753f959c14f596ff8971892
Wael wahbeh <waelio@users.noreply.github.com>
2021-01-01 20:32:40 +0200

    extreame makeover

---
+++ src/boot/register-waelio-ext.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import VuePlugin from '../quasar-app-extension-waelio/templates/plugin'
+import Component from '../components/Component.vue'
+
+Vue.component('Lang-Switch', Component)
+
+Vue.use(VuePlugin)
