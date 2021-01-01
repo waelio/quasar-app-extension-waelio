3675b756c75bfb2220888564983e9f059a1c107e
Wael wahbeh <waelio@users.noreply.github.com>
2021-01-01 12:57:07 +0200

    another try

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ module.exports = function (api) {
   api.compatibleWith('quasar', '^1.1.1')
   api.compatibleWith('@quasar/app', '^1.1.0 || ^2.0.0')
 
-  api.extendQuasarConf(extendConf)
+  // api.extendQuasarConf(extendConf)
+  api.framework.config.dark = 'auto'
+  api.framework.plugins.push('Notify', 'Loading', 'Dialog', 'Meta', 'Cookies', 'LocalStorage', 'SessionStorage') 
+  api.build.vueRouterMode = 'history'
+  api.extendPackageJson({
+    scripts: {
+      'spa': 'quasar dev -m spa',
+      'pwa': 'quasar dev -m pwa',
+      'ssr': 'quasar dev -m ssr',
+      'ios': 'quasar dev -m ios',
+      'android': 'quasar dev -m android',
+      'nextPatch': 'npm version patch',
+    }
+  })
+  api.pages.push('~quasar-app-extension-waelio/src/components/Component.vue')
+  api.pages.push('~quasar-app-extension-waelio/src/pages/AboutUs.vue')
+  api.pages.push('~quasar-app-extension-waelio/src/pages/ContactUs.vue')
+  api.pages.push('~quasar-app-extension-waelio/src/pages/Privacy.vue')
+  api.pages.push('~quasar-app-extension-waelio/src/pages/Terms.vue')
+  api.pages.push('~quasar-app-extension-waelio/src/pages/Settings.vue')
+
+  api.boot.push('~quasar-app-extension-waelio/src/boot/register.js')
+  api.build.transpileDependencies.push(/quasar-app-extension-waelio[\\/]src/)
 }
 function extendConf (conf) {
   conf.boot.push('~quasar-app-extension-waelio/src/boot/register.js')
--- src/templates/register.js
+++ src/templates/register.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
-import VuePlugin from 'quasar-app-extension-waelio'
+import VuePlugin from 'quasar-app-extension-waelio/templates/plugin'
 
 Vue.use(VuePlugin)
