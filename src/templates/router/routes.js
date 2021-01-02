ebf11920ff1ef657062a5a1213f54660546b6e85
Wael wahbeh <waelio@users.noreply.github.com>
2021-01-02 14:20:36 +0200

    my bad

--- src/templates/router/routes.js
+++ src/templates/router/routes.js
@@ -6,10 +6,7 @@ const routes = [
     children: [
       { path: ''         , name: '',           component: () => import('pages/Index.vue') },
       { path: 'home'     , name: 'home',       component: () => import('pages/Index.vue') },
-      { path: 'auth'     , name: 'auth',       component: () => import('pages/Auth.vue') },
       { path: 'about'    , name: 'about',      component: () => import('pages/AboutUs.vue') },
-      { path: 'services' , name: 'services',   component: () => import('pages/Services.vue') },
-      { path: 'products' , name: 'products',   component: () => import('pages/Products.vue') },
       { path: 'contact'  , name: 'contact',    component: () => import('pages/ContactUs.vue') },
       { path: 'privacy'  , name: 'privacy',    component: () => import('pages/Privacy.vue') },
       { path: 'terms'    , name: 'terms',      component: () => import('pages/Terms.vue') },
