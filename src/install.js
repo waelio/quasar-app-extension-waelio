2315725444c69eb1f889468dcad8cfb50afa7870
Wael wahbeh <waelio@users.noreply.github.com>
2021-01-23 23:39:05 +0200

    adding copy-webpack-plugin again

--- src/install.js
+++ src/install.js
@@ -16,7 +16,7 @@ function RunCommand(installCommand) {
 }
 
 var installCommand = "npm i --force --save emailjs-com universal-config waelio-utils";
-var installDev = "npm i --force -D sitemap-webpack-plugin";
+var installDev = "npm i --force -D sitemap-webpack-plugin copy-webpack-plugin";
 console.log(chalk.yellow("Attempting Dependencies: ") + chalk.green(installCommand));
 console.log(chalk.yellow("Attempting DevDependencies:") + chalk.green(installDev));
 RunCommand("npm install -D chalk");
