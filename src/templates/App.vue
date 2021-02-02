eab4c0c1263ca04fb6b333f0457a03b77402c316
Wael wahbeh <waelio@users.noreply.github.com>
2021-02-03 00:04:40 +0200

    add rtl support

--- src/templates/App.vue
+++ src/templates/App.vue
@@ -5,30 +5,48 @@
   </div>
 </template>
 <script>
-import { meta} from "waelio-utils";
-import DeBugger from "./components/DeBugger";
+import { meta } from 'waelio-utils'
+import DeBugger from './components/DeBugger'
 export default {
   components: { DeBugger },
-  name: "App",
-  mounted() {
-    this.verifyLocale();
-    this.verifyMode();
+  name: 'App',
+  mounted () {
+    this.verifyLocale()
+    this.verifyMode()
   },
-  data() {
+  data () {
     return {
       metaTags: {
-        title: this.$config.get("app:businessName"),
-        description:this.$config.get("app:businessDescription"),
-        url: this.$config.get("app:businessDomain"),
-        image: this.$config.get("app:businessImage")
+        title: this.$config.get('app:businessName'),
+        description: this.$config.get('app:businessDescription'),
+        url: this.$config.get('app:businessDomain'),
+        image: this.$config.get('app:businessImage')
       },
       meta
-    };
+    }
   },
   computed: {
-    listeners() {
-      return this.$listeners;
+    listeners () {
+      return this.$listeners
     }
   }
-};
+}
 </script>
+<style lang="scss">
+body.ar,
+body.he {
+  direction: rtl;
+  p,
+  li,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  div,
+  span,
+  a {
+    direction: rtl;
+  }
+}
+</style>
